3820adf8e07f4c021678c985443557b1ccac7377
Cedric Benonga <cedricbenonga13@gmail.com>
2023-08-31 23:30:09 +0200

    Updated

--- main.py
+++ main.py
@@ -2,26 +2,34 @@ import pandas
 
 payops_team = [
     "Memory M",
-    "Zione B",
-    "WalterK",
-    "Petronella K",
-    "SibongileP",
     "Tsoanelo B",
     "Jane B",
-    "Mirriam N",
-    "Matshidiso M",
     "MphoM",
     "Lehlomela",
     "Rose K",
     "Denzel M",
-    "Penina M",
     "Wina Y",
+    "Zione B",
+    "WalterK",
+    "Petronella K",
+    "SibongileP",
+    "Matshidiso M",
+    "Mirriam N",
+    "Penina M",
     "Pontsho H"
 ]
 
-data = pandas.read_csv("Openchannel By Agent Samuel-iaPq.csv")
+try:
+    data = pandas.read_csv("Openchannel By Agent Samuel-iaPq.csv")
+    # data.to_csv
+except FileNotFoundError:
+    print('Sorry! File name not found!\nPlease check the "File Name"(it needs to be the same with the one in the '
+          'database) or \n"File Format"(only csv accepted) and retry!')
 
-for name in payops_team:
-    if name in data.Agent.to_list():
-        new_data = data[data.Agent == name]
-        print(f'{new_data.Agent.to_list()[0]}: {sum(new_data["Total Interactions"].to_list())}')
+try:
+    for name in payops_team:
+        if name in data.Agent.to_list():
+            new_data = data[data.Agent == name]
+            print(f'{new_data.Agent.to_list()[0]}: {sum(new_data["Total Interactions"].to_list())}')
+except NameError:
+    pass
